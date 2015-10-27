befdd88e6029e94da3c2d047a70a2166284959f0
Raul <raulvidana@hotmail.com>
2015-10-27 15:37:17 -0600

    Proyecto planetas

---
+++ Mi.Biblioteca/Assets/Scripts/planeta.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class planeta : MonoBehaviour {
+	public GameObject center;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(center.transform.position , Vector3.up, speed * Time.deltaTime);
+	
+	}
+}
